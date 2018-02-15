81324d44237c7c9143901b7101848d0734c919fa
moritaka <moritaka@ff-network.com>
2018-02-15 12:53:42 +0900

    add files

---
+++ files/copy.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import shutil
+
+def main():
+
+    args = sys.argv
+    argc = len(args)
+
+    if argc != 2:
+        print("need 1 argument.")
+        return
+
+    for n in range(int(args[1]) - 1 ):
+        shutil.copyfile("./1.txt" , "./" + str(n+2) + ".txt")
+
+    return
+
+
+if __name__ == '__main__':
+    main()
